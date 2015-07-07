cb60231dc0cafb20af64bf6b3cb619d435290d96
Elainer Toribio <toribioelainer@gmail.com>
2015-07-06 23:57:56 -0400

    Cambiado layout

---
+++ 3er Parcial/3er Parcial/CanInheritPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3er_Parcial
+{
+    /// <summary>
+    /// Interaction logic for CanInheritPage.xaml
+    /// </summary>
+    public partial class CanInheritPage : Page
+    {
+        public CanInheritPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
