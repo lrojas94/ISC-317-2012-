f7f9ce2b31ceaa53274ef587b0473c2f85328e02
lrojas94 <lrojas94@gmail.com>
2015-07-07 04:05:01 -0400

    Finalizado con Enemigos

--- 3er Parcial/3er Parcial/EnemiesPage.xaml.cs
+++ 3er Parcial/3er Parcial/EnemiesPage.xaml.cs
@@ -23,7 +23,41 @@ namespace _3er_Parcial
         public EnemiesPage()
         {
             InitializeComponent();
-            
+            this.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+            this.Arrange(new Rect(0, 0, this.DesiredSize.Width, this.DesiredSize.Height));
+
+        }
+
+        private void testEnemies_Click(object sender, RoutedEventArgs e)
+        {
+
+            characterImageA.Source = null;
+            characterImageB.Source = null;
+            result.Source = null;
+
+            Character charA = CharactersModel.Instance.FindCharacterWith(characterATB.Text);
+            Character charB = CharactersModel.Instance.FindCharacterWith(characterBTB.Text);
+            if (charA != null && charB != null)
+            {
+                characterImageA.Source = charA.HouseImage;
+                characterImageB.Source = charB.HouseImage;
+                Uri uri;
+                if (CharactersModel.Instance.AreEnemies(charA, charB))
+                    uri = new Uri("pack://application:,,,/Images/enemies.png");
+                else
+                    uri = new Uri("pack://application:,,,/Images/friends.png");
+
+                BitmapImage image = new BitmapImage(uri);
+                result.Source = image;
+            }
+            else {
+                string message;
+                if (charA == null)
+                    message = String.Format("No se ha encontrado el personaje {0}", characterATB.Text);
+                else
+                    message = String.Format("No se ha encontrado el personaje {0}", characterBTB.Text);
+                MessageBox.Show(message);
+            }
         }
     }
 }
